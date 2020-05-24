3d3edd9628bd01919a167c2b2177a6bfade72c89
pranavv481 <pranavv481@gmail.com>
2020-05-24 11:54:54 +0530

    portal

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+import PortalDemo from './components/PortalDemo';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <PortalDemo />
+
+      </div>
+    );
+  }
+}
+
+export default App;
